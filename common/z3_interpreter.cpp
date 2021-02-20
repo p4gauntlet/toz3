9e90acbec58331e65652b7ea258dc26b14c4856d
fruffy <fruffy@nyu.edu>
2021-02-20 12:03:13 -0500

    Revert "Move expression resolver into separate visitor."
    This reverts commit 7f28e875bf676c4ebd276afc9deb3b94d33151b6.

--- common/z3_interpreter.cpp
+++ common/z3_interpreter.cpp
@@ -2,7 +2,6 @@
 #include <utility>
 
 #include "complex_type.h"
-#include "expression_resolver.h"
 #include "ir/ir-generated.h"
 #include "lib/exceptions.h"
 #include "z3_int.h"
@@ -14,6 +13,7 @@ Visitor::profile_t Z3Visitor::init_apply(const IR::Node *node) {
     return Inspector::init_apply(node);
 }
 
+void Z3Visitor::end_apply(const IR::Node *) {}
 
 void Z3Visitor::fill_with_z3_sorts(std::vector<const IR::Node *> *sorts,
                                    const IR::Type *t) {
@@ -25,6 +25,29 @@ bool Z3Visitor::preorder(const IR::P4Control *c) {
     // DO SOMETHING
     visit(c->body);
     return false;
+}
+
+P4Z3Instance Z3Visitor::cast(P4Z3Instance expr, const IR::Type *dest_type) {
+    if (auto tb = dest_type->to<IR::Type_Bits>()) {
+        if (z3::expr *z3_var = boost::get<z3::expr>(&expr)) {
+            if (z3_var->get_sort().is_bv()) {
+                return state->ctx->bv_val(z3_var->get_decimal_string(0).c_str(),
+                                          dest_type->width_bits());
+            } else if (z3_var->get_sort().is_bool()) {
+
+            } else {
+            }
+
+        } else if (auto z3_var = check_complex<Z3Int>(expr)) {
+            auto val_string = Util::toString(z3_var->val, 0, false);
+            return state->ctx->bv_val(val_string, dest_type->width_bits());
+        } else {
+            BUG("Cast from expr xpr to node %s supported ",
+                dest_type->node_type_name());
+        }
+    } else {
+        BUG("Cast to type %s not supported", dest_type->node_type_name());
+    }
 }
 
 P4Z3Instance Z3Visitor::resolve_member(const IR::Member *m) {
@@ -53,10 +76,8 @@ bool Z3Visitor::preorder(const IR::BlockStatement *b) {
 }
 
 bool Z3Visitor::preorder(const IR::AssignmentStatement *as) {
-    ExprResolver expr_resolver(state, this);
-    as->right->apply(expr_resolver);
+    visit(as->right);
     auto var = state->return_expr;
-
     auto target = as->left;
     if (auto name = target->to<IR::PathExpression>()) {
         state->insert_var(name->path->name, var);
@@ -82,6 +103,85 @@ bool Z3Visitor::preorder(const IR::AssignmentStatement *as) {
     return false;
 }
 
+bool Z3Visitor::preorder(const IR::Constant *c) {
+    auto val_string = Util::toString(c->value, 0, false);
+    if (auto tb = c->type->to<IR::Type_Bits>()) {
+        if (tb->isSigned) {
+            state->return_expr = new Z3Int(c->value, tb->size);
+        } else {
+            state->return_expr = state->ctx->bv_val(val_string, tb->size);
+        }
+        return false;
+    } else if (c->type->is<IR::Type_InfInt>()) {
+        state->return_expr = new Z3Int(c->value, -1);
+        return false;
+    }
+    BUG("Constant Node %s not implemented!", c->type->node_type_name());
+}
+
+bool Z3Visitor::preorder(const IR::PathExpression *p) {
+    P4Scope *scope;
+    state->return_expr = state->find_var(p->path->name, &scope);
+    return false;
+}
+
+bool Z3Visitor::preorder(const IR::Cast *c) {
+    // resolve expression
+    visit(c->expr);
+    auto resolved_expr = state->return_expr;
+    state->return_expr = cast(resolved_expr, c->destType);
+
+    return false;
+}
+
+bool Z3Visitor::preorder(const IR::ConstructorCallExpression *cce) {
+    const IR::Type *resolved_type = state->resolve_type(cce->constructedType);
+    std::vector<std::pair<cstring, z3::expr>> state_vars;
+    if (auto c = resolved_type->to<IR::P4Control>()) {
+        auto scope = new P4Scope();
+        state->add_scope(scope);
+        std::vector<cstring> state_names;
+        // INITIALIZE
+        for (auto param : *c->getApplyParameters()) {
+            auto par_type = state->resolve_type(param->type);
+            P4Z3Instance var = state->gen_instance(param->name.name, par_type);
+            state->insert_var(param->name.name, var);
+            state_names.push_back(param->name.name);
+        }
+
+        // VISIT THE CONTROL
+        visit(resolved_type);
+
+        // COLLECT
+        for (auto state_name : state_names) {
+            P4Scope *scope;
+            auto member = state->find_var(state_name, &scope);
+            if (z3::expr *z3_var = boost::get<z3::expr>(&member)) {
+                state_vars.push_back({state_name, *z3_var});
+            } else if (auto z3_var = check_complex<StructInstance>(member)) {
+                auto z3_sub_vars = z3_var->get_z3_vars(state_name);
+                state_vars.insert(state_vars.end(), z3_sub_vars.begin(),
+                                  z3_sub_vars.end());
+            } else if (auto z3_var = check_complex<ErrorInstance>(member)) {
+                auto z3_sub_vars = z3_var->get_z3_vars(state_name);
+                state_vars.insert(state_vars.end(), z3_sub_vars.begin(),
+                                  z3_sub_vars.end());
+            } else if (auto z3_var = check_complex<EnumInstance>(member)) {
+                auto z3_sub_vars = z3_var->get_z3_vars(state_name);
+                state_vars.insert(state_vars.end(), z3_sub_vars.begin(),
+                                  z3_sub_vars.end());
+            } else if (check_complex<ExternInstance>(member)) {
+                printf("Skipping extern...\n");
+            } else {
+                BUG("Var is neither type z3::expr nor P4ComplexInstance!");
+            }
+        }
+    }
+    state->return_expr = new ControlState(state_vars);
+
+    return false;
+}
+
 P4Z3Result
 Z3Visitor::merge_args_with_params(const IR::Vector<IR::Argument> *args,
                                   const IR::ParameterList *params) {
@@ -91,8 +191,7 @@ Z3Visitor::merge_args_with_params(const IR::Vector<IR::Argument> *args,
     for (auto param : params->parameters) {
         if (idx < arg_len) {
             const IR::Argument *arg = args->at(idx);
-            ExprResolver expr_resolver(state, this);
-            arg->expression->apply(expr_resolver);
+            visit(arg->expression);
             merged_vec.push_back({param->name.name, state->return_expr});
         } else {
             auto arg_expr = state->gen_instance(param->name.name, param->type);
@@ -100,6 +199,7 @@ Z3Visitor::merge_args_with_params(const IR::Vector<IR::Argument> *args,
         }
         idx++;
     }
+
     return merged_vec;
 }
 
