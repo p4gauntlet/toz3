325eea64bef1d253bb1099d31969d07b37f666e7
fruffy <fruffy@nyu.edu>
2021-04-22 19:47:00 +0200

    Make error printing a little bit prettier.

--- compare/main.cpp
+++ compare/main.cpp
@@ -1,6 +1,10 @@
+#include <cstddef>
 #include <cstdio>
 #include <cstdlib>
 #include <fstream>
+#include <iomanip>
+#include <iterator>
+#include <ostream>
 
 #include "frontends/common/applyOptionsPragmas.h"
 #include "frontends/common/constantFolding.h"
@@ -31,6 +35,9 @@
 #include "toz3_v2/common/visitor_fill_type.h"
 #include "toz3_v2/common/visitor_interpret.h"
 
+using Z3Prog = std::pair<cstring, std::vector<std::pair<cstring, z3::expr>>>;
+constexpr auto COLUMN_WIDTH = 40;
+
 namespace TOZ3_V2 {
 
 const IR::Declaration_Instance *get_main_decl(TOZ3_V2::P4State *state) {
@@ -50,42 +57,42 @@ VarMap get_z3_repr(cstring prog_name, const IR::P4Program *program,
                    z3::context *ctx) {
     VarMap z3_return;
 
-    if (program != nullptr && ::errorCount() == 0) {
-        try {
-            // convert the P4 program to Z3
-            P4State state(ctx);
-            TypeVisitor map_builder = TypeVisitor(&state);
-            Z3Visitor to_z3 = Z3Visitor(&state);
-            program->apply(map_builder);
-            const auto *decl = get_main_decl(&state);
-            if (decl == nullptr) {
-                return z3_return;
-            }
-            return to_z3.gen_state_from_instance(decl);
-        } catch (const Util::P4CExceptionBase &bug) {
-            std::cerr << "Failed to interpret pass \"" << prog_name << "\"."
-                      << std::endl;
-            std::cerr << bug.what() << std::endl;
-            exit(EXIT_FAILURE);
-        } catch (z3::exception &ex) {
-            std::cerr << "Failed to interpret pass \"" << prog_name << "\"."
-                      << std::endl;
-            std::cerr << "Z3 exception: " << ex << std::endl;
-            exit(EXIT_FAILURE);
+    try {
+        // convert the P4 program to Z3
+        P4State state(ctx);
+        TypeVisitor map_builder = TypeVisitor(&state);
+        Z3Visitor to_z3 = Z3Visitor(&state);
+        program->apply(map_builder);
+        const auto *decl = get_main_decl(&state);
+        if (decl == nullptr) {
+            return z3_return;
         }
+        return to_z3.gen_state_from_instance(decl);
+    } catch (const Util::P4CExceptionBase &bug) {
+        std::cerr << "Failed to interpret pass \"" << prog_name << "\"."
+                  << std::endl;
+        std::cerr << bug.what() << std::endl;
+        exit(EXIT_FAILURE);
+    } catch (z3::exception &ex) {
+        std::cerr << "Failed to interpret pass \"" << prog_name << "\"."
+                  << std::endl;
+        std::cerr << "Z3 exception: " << ex << std::endl;
+        exit(EXIT_FAILURE);
     }
     return z3_return;
 }
 
 void unroll_result(const VarMap &z3_repr_prog,
-                   std::vector<z3::expr> *result_vec) {
+                   std::vector<std::pair<cstring, z3::expr>> *result_vec) {
     for (const auto &result_tuple : z3_repr_prog) {
-        if (const auto *z3_val = result_tuple.second.first->to<NumericVal>()) {
-            result_vec->push_back(*z3_val->get_val());
-        } else if (const auto *z3_var =
-                       result_tuple.second.first->to<ControlState>()) {
-            for (const auto &sub_tuple : z3_var->state_vars) {
-                result_vec->push_back(sub_tuple.second);
+        auto name = result_tuple.first;
+        auto z3_result = result_tuple.second;
+        if (const auto *num_var = z3_result.first->to<NumericVal>()) {
+            result_vec->push_back({name, *num_var->get_val()});
+        } else if (const auto *ctrl_var = z3_result.first->to<ControlState>()) {
+            for (const auto &sub_tuple : ctrl_var->state_vars) {
+                auto sub_name = name + "_" + sub_tuple.first;
+                result_vec->push_back({sub_name, sub_tuple.second});
             }
         } else {
             P4C_UNIMPLEMENTED("Unsupported result type.");
@@ -93,69 +100,81 @@ void unroll_result(const VarMap &z3_repr_prog,
     }
 }
 
-int compare_progs(
-    z3::context *ctx,
-    std::vector<std::pair<cstring, std::vector<z3::expr>>> z3_progs) {
+z3::expr
+create_z3_struct(z3::context *ctx,
+                 const std::vector<std::pair<cstring, z3::expr>> &z3_prog) {
+    z3::expr_vector z3_vec(*ctx);
+    std::vector<z3::sort> z3_vec_sorts;
+    std::vector<const char *> names;
+    z3::func_decl_vector getters(*ctx);
+
+    for (const auto &prog_tuple : z3_prog) {
+        cstring before_name = "before" + prog_tuple.first;
+        names.push_back(before_name.c_str());
+        z3_vec.push_back(prog_tuple.second);
+        z3_vec_sorts.push_back(prog_tuple.second.get_sort());
+    }
+    auto before_sort = ctx->tuple_sort("State", z3_vec.size(), names.data(),
+                                       z3_vec_sorts.data(), getters);
+
+    return before_sort(z3_vec);
+}
+
+void print_violation_error(const z3::solver &s, const Z3Prog &prog_before,
+                           const Z3Prog &prog_after) {
+    std::cerr << "\n\nPrograms are not equal! Found validation error.\n";
+    std::cerr << "Program " << prog_before.first << " before:\n";
+    for (const auto &prog_tuple_before : prog_before.second) {
+        cstring left_name = prog_tuple_before.first + ": ";
+        std::cerr << std::left << std::setw(COLUMN_WIDTH) << left_name;
+        std::cerr << std::right << std::setw(COLUMN_WIDTH)
+                  << prog_tuple_before.second.simplify() << std::endl;
+    }
+    std::cerr << "\nProgram " << prog_after.first << " after:\n";
+    for (const auto &prog_tuple_after : prog_after.second) {
+        cstring left_name = prog_tuple_after.first + ": ";
+        std::cerr << std::left << std::setw(COLUMN_WIDTH) << left_name;
+        std::cerr << std::right << std::setw(COLUMN_WIDTH)
+                  << prog_tuple_after.second.simplify() << std::endl;
+    }
+    auto model = s.get_model();
+    std::cerr << "\nSolution :\n";
+    for (size_t idx = 0; idx < model.size(); idx++) {
+        auto var = model[idx];
+        std::cerr << var.name() << " = " << model.get_const_interp(var)
+                  << std::endl;
+    }
+}
+
+int compare_progs(z3::context *ctx, const std::vector<Z3Prog> &z3_progs) {
     z3::solver s(*ctx);
-
+    auto prog_before = z3_progs[0];
+    auto z3_prog_before = create_z3_struct(ctx, prog_before.second);
     for (size_t i = 1; i < z3_progs.size(); ++i) {
+        auto prog_after_name = z3_progs[i].first;
+        std::cout << "Comparing " << prog_before.first << " and "
+                  << prog_after_name << "." << std::endl;
+        auto prog_after = z3_progs[i];
+        auto z3_prog_after = create_z3_struct(ctx, z3_progs[i].second);
+
         s.push();
-        cstring prog_before_name = z3_progs[i - 1].first;
-        cstring prog_after_name = z3_progs[i].first;
-        printf("Comparing %s and %s \n", prog_before_name.c_str(),
-               prog_after_name.c_str());
-        std::vector<z3::expr> z3_repr_prog_before = z3_progs[i - 1].second;
-        std::vector<z3::expr> z3_repr_prog_after = z3_progs[i].second;
-        z3::expr_vector z3_vec_before(*ctx);
-        z3::expr_vector z3_vec_after(*ctx);
-        std::vector<z3::sort> z3_vec_before_sorts;
-        std::vector<z3::sort> z3_vec_after_sorts;
-        std::vector<const char *> before_names;
-        std::vector<const char *> after_names;
-        z3::func_decl_vector before_getters(*ctx);
-        z3::func_decl_vector after_getters(*ctx);
-
-        for (size_t i = 0; i < z3_repr_prog_before.size(); ++i) {
-            cstring before_name = "before" + std::to_string(i);
-            cstring after_name = "after" + std::to_string(i);
-            before_names.push_back(before_name.c_str());
-            after_names.push_back(after_name.c_str());
-            z3_vec_before.push_back(z3_repr_prog_before[i]);
-            z3_vec_after.push_back(z3_repr_prog_after[i]);
-            z3_vec_before_sorts.push_back(z3_repr_prog_after[i].get_sort());
-            z3_vec_after_sorts.push_back(z3_repr_prog_after[i].get_sort());
-        }
-        z3::func_decl before_sort =
-            ctx->tuple_sort("State", z3_vec_before.size(), before_names.data(),
-                            z3_vec_before_sorts.data(), before_getters);
-
-        z3::func_decl after_sort =
-            ctx->tuple_sort("State", z3_vec_after.size(), after_names.data(),
-                            z3_vec_after_sorts.data(), after_getters);
-        z3::expr prog_before = before_sort(z3_vec_before);
-        z3::expr prog_after = after_sort(z3_vec_after);
-
-        s.add(prog_before != prog_after);
+        s.add(z3_prog_before != z3_prog_after);
         std::cout << "Checking... " << std::endl;
         auto ret = s.check();
         std::cout << "Result: " << ret << std::endl;
         if (ret == z3::sat) {
-            error("Programs are not equal! Found validation error.\n");
-            error("Program %s before %s", prog_before_name,
-                  prog_before.simplify().to_string().c_str());
-            error("Program %s after %s", prog_after_name,
-                  prog_after.simplify().to_string().c_str());
-            auto m = s.get_model();
-            std::cerr << "Solution : " << m << std::endl;
+            print_violation_error(s, prog_before, prog_after);
             return EXIT_VIOLATION;
         }
         if (ret == z3::unknown) {
-            error("Could not determine equality. Error\n");
+            std::cerr << "Error: Could not determine equality. Error"
+                      << std::endl;
             return EXIT_FAILURE;
         }
         s.pop();
+        prog_after = prog_before;
     }
-    printf("Passed all checks.\n");
+    std::cout << "Passed all checks." << std::endl;
     return EXIT_SUCCESS;
 }
 }  // namespace TOZ3_V2
@@ -200,28 +219,27 @@ int main(int argc, char *const argv[]) {
     }
     auto prog_list = split_input_progs(options.file);
     if (prog_list.size() < 2) {
-        error("At least two input programs expected.");
+        std::cerr << "At least two input programs expected." << std::endl;
         options.usage();
         return EXIT_FAILURE;
     }
 
     z3::context ctx;
-    const IR::P4Program *prog_parsed = nullptr;
-    // parse the first program
-    // use a little trick here to get the second program
-    std::vector<std::pair<cstring, std::vector<z3::expr>>> z3_progs;
+    // Parse the first program
+    // Use a little trick here to get the second program
+    std::vector<Z3Prog> z3_progs;
     for (auto prog : prog_list) {
         options.file = prog;
-        prog_parsed = P4::parseP4File(options);
+        const auto *prog_parsed = P4::parseP4File(options);
         if (prog_parsed != nullptr && ::errorCount() == 0) {
             P4::P4COptionPragmaParser optionsPragmaParser;
             prog_parsed->apply(P4::ApplyOptionsPragmas(optionsPragmaParser));
         } else {
-            error("Unable to parse program.");
+            std::cerr << "Unable to parse program." << std::endl;
             return EXIT_FAILURE;
         }
         auto z3_repr_prog = TOZ3_V2::get_z3_repr(prog, prog_parsed, &ctx);
-        std::vector<z3::expr> result_vec;
+        std::vector<std::pair<cstring, z3::expr>> result_vec;
         TOZ3_V2::unroll_result(z3_repr_prog, &result_vec);
         z3_progs.emplace_back(prog, result_vec);
     }
