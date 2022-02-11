0a31c31461838cf2d101a8ba5cea8c069bc6af48
Anish Agrawal <aa8143@nyu.edu>
2022-02-11 14:50:16 -0500

    Push new test

--- pruner/example_programs/fabric_with_creduce/test.sh
+++ pruner/example_programs/fabric_with_creduce/test.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-/home/roborobo/projects/gauntlet_repo/gauntlet/modules/toz3/pruner/tests/broken_p4c/24895c1b28a35352f1d9ad1f43878c4f7061d3ab -I /home/roborobo/projects/gauntlet_repo/gauntlet/modules/toz3/pruner/example_programs/fabric_with_creduce/ ./fabric.p4 2>&1 > /dev/null | grep "Compiler Bug"
+CUR_DIR=`pwd`
+${CUR_DIR}/../../tests/broken_p4c/24895c1b28a35352f1d9ad1f43878c4f7061d3ab -I $CUR_DIR ./fabric.p4 2>&1 > /dev/null | grep "Compiler Bug"
